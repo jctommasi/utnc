f2199f213eee1b70d6824a104a7ffd44a50dc9fc
alumno <alumno@LAB00PC00>
2019-09-04 21:08:27 -0300

    arrays

---
+++ functions/src/functions.c
@@ -0,0 +1,146 @@
+/*
+ ============================================================================
+ Name        : functions.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+funcion para minimo, maximo, promedio suma
+
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio_ext.h>
+
+#define QLY_INPUTS 5
+int initArrayInt(int *pArray , int limite, int valor);
+int printArrayInt(int *pArray , int limite);
+int getArrayInt(	int *pArray,
+					int limite,
+					char *pMensaje,
+					char *pMensajeError,
+					int minimo,
+					int maximo,
+					int reintentos);
+
+int getInt(	int *pResultado,
+			char *pMensaje,
+			char *pMensajeError,
+			int minimo,
+			int maximo,
+			int reintentos);
+
+int main(void)
+{
+	int inputs[QLY_INPUTS] = {100,200,300,400,500};
+	int cantidadUsuario;
+	initArrayInt(inputs,QLY_INPUTS,22);
+	cantidadUsuario = getArrayInt(inputs,QLY_INPUTS,"Ingrese un numero?\n","Error\n",0,200,2);
+
+	printArrayInt(inputs,cantidadUsuario);
+	return EXIT_SUCCESS;
+}
+
+int initArrayInt(int *pArray , int limite, int valor)
+{
+	int retorno = -1;
+	int i;
+	if(pArray != NULL && limite > 0)
+	{
+		for(i=0;i<limite;i++)
+		{
+			pArray[i]=valor;
+		}
+		retorno = 0;
+	}
+	return retorno;
+}
+
+
+
+int printArrayInt(int *pArray , int limite)
+{
+	int retorno = -1;
+	int i;
+	printf("\nDEBUG\n");
+	if(pArray != NULL && limite > 0)
+	{
+		for(i=0;i<limite;i++)
+		{
+			printf("\n\t%i",pArray[i]);
+		}
+		retorno = 0;
+	}
+	return retorno;
+}
+
+int getArrayInt(int *pArray,
+				int limite,
+				char *pMensaje,
+				char *pMensajeError,
+				int minimo,
+				int maximo,
+				int reintentos)
+{
+	int retorno = -1;
+	int buffer;
+	int i=0;
+	char respuesta;
+	printf("\nDEBUG\n");
+	if(pArray != NULL && limite > 0)
+	{
+		do
+		{
+
+			if(	getInt(	&buffer,
+						pMensaje,
+						pMensajeError,
+						minimo,
+						maximo,
+						reintentos) == 0)
+			{
+				pArray[i] = buffer;
+				i++;
+				if(i == limite)
+				{
+					break;
+				}
+			}
+
+			printf("Continuar (s/n)? \n");
+			__fpurge(stdin); // fflush(stdin)
+			scanf("%c",&respuesta);
+		}while(respuesta != 'n');
+		retorno = i;
+	}
+	return retorno;
+}
+
+int getInt(	int *pResultado,
+			char *pMensaje,
+			char *pMensajeError,
+			int minimo,
+			int maximo,
+			int reintentos)
+{
+	int retorno = -1;
+	int buffer;
+
+	do
+	{
+		printf("%s",pMensaje);
+		__fpurge(stdin);
+		if(scanf("%d",&buffer)==1 && buffer >= minimo && buffer <= maximo)
+		{
+			*pResultado = buffer;
+			retorno = 0;
+			break;
+		}
+		printf("%s",pMensajeError);
+		reintentos--;
+	} while(reintentos >= 0);
+	return retorno;
+}
